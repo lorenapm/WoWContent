a8756ef89e2249de66e576b8aeead4826bcf5484
Lorena <lorena.perez@gmail.com>
2021-10-02 14:42:49 +0200

    create navbar

---
+++ src/WowApp.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import { Header } from "./components/Header";
+
+const WowApp = () => {
+  return <Header />;
+};
+
+export default WowApp;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import WowApp from "./WowApp";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WowApp />
   </React.StrictMode>,
   document.getElementById("root")
 );
