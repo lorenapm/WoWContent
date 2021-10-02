902e26de2c232620542510e312c12d6b290a9e2d
Lorena <lorena.perez@gmail.com>
2021-10-02 16:18:24 +0200

    clean navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,36 @@
 import React from "react";
+import ReactDom from "react-dom";
 import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 export function Header() {
   return (
     <>
-  <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+  <Navbar collapseOnSelect expand="lg" bg="light" variant="light" sticky="top">
   <Container>
   <Navbar.Brand href="#home">Wow Content</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="me-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-      <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      <Nav.Link href="#features">Home</Nav.Link>
+      <Nav.Link href="#features">¿Por qué nosotros?</Nav.Link>
+      <NavDropdown title="¿Qué hacemos?" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Presentaciones</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Infografías</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Vídeo presetaciones</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Guías corporativas</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Material formativo</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Contenidos RRSS</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Imagen de marca</NavDropdown.Item>
       </NavDropdown>
+      <Nav.Link href="#pricing">Portfolio</Nav.Link>
     </Nav>
     <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
+      <Nav.Link href="#deets">Contacto</Nav.Link>
     </Nav>
   </Navbar.Collapse>
   </Container>
 </Navbar>
+
 </>
   );
 }
