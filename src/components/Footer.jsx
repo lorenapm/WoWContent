b342191f2904be6f8af78d4b4faeee442133e6e3
Lorena <lorena.perez@gmail.com>
2021-10-04 13:30:50 +0200

    clean footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,47 +4,15 @@ import { Container } from "react-bootstrap";
 
 export const Footer = () => {
     return (
-<Container className="page-footer font-small blue-grey lighten-5">
-
- 
+<Container className="page-footer font-small bg-light">
   <div className="container text-center text-md-left mt-5">
-
-    
     <div className="row mt-3 dark-grey-text">
-
-     
       <div className="col-md-3 col-lg-4 col-xl-3 mb-4">
-
-      
         <h6 className="text-uppercase font-weight-bold">Company name</h6>
         <hr className="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style={{width: 60}}/>
         <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet adipisicing elit.</p>
-
       </div>
-     
-      <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-
-       
-        <h6 className="text-uppercase font-weight-bold">Products</h6>
-        <hr className="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style={{width: 60}} />
-        <p>
-          <a className="dark-grey-text" href="#!">MDBootstrap</a>
-        </p>
-        <p>
-          <a className="dark-grey-text" href="#!">MDWordPress</a>
-        </p>
-        <p>
-          <a className="dark-grey-text" href="#!">BrandFlow</a>
-        </p>
-        <p>
-          <a className="dark-grey-text" href="#!">Bootstrap Angular</a>
-        </p>
-
-      </div>
-     
       <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-
-      
         <h6 className="text-uppercase font-weight-bold">Useful links</h6>
         <hr className="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style={{width: 60}} />
         <p>
@@ -54,17 +22,12 @@ export const Footer = () => {
           <a className="dark-grey-text" href="#!">Become an Affiliate</a>
         </p>
         <p>
-          <a className="dark-grey-text" href="#!">Shipping Rates</a>
-        </p>
-        <p>
           <a className="dark-grey-text" href="#!">Help</a>
         </p>
 
       </div>
     
       <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-
-      
         <h6 className="text-uppercase font-weight-bold">Contact</h6>
         <hr className="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style={{width: 60}}/>
         <p>
@@ -73,20 +36,13 @@ export const Footer = () => {
           <i className="fas fa-envelope mr-3"></i> info@example.com</p>
         <p>
           <i className="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-        <p>
-          <i className="fas fa-print mr-3"></i> + 01 234 567 89</p>
-
       </div>
- 
-
     </div>
- 
-
   </div>
  
-  <div className="footer-copyright text-center text-black-50 py-3">© 2020 Oh! Presentaciones</div>
- 
-
+  <div className="copyright-box bg-secondary text-center text-white py-3">
+    © 2021 Oh! Presentaciones
+  </div>
 </Container>
 
     )};
