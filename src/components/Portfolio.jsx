53f58b08f6d5fe85be2498656661be696becb454
Lorena <lorena.perez@gmail.com>
2021-10-07 18:57:08 +0200

    clean media

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,12 +1,20 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Ratio } from "react-bootstrap";
 
 export function Portfolio() {
     return (
-    <Container>
+        <>
+<h2 className="title1">Para muestra, un botón...</h2>
+
+    <Container className="box-portfolio">
   <Row>
-    <Col xs>First, but unordered</Col>
-    <Col xs={{ order: 12 }}>Second, but last</Col>
-    <Col xs={{ order: 1 }}>Third, but second</Col>
+    <Col md="8">
+    <Ratio aspectRatio="16x9">
+  <iframe frameBorder="0" src="https://view.genial.ly/6159ab4c7deef00e1f33dbd9" type="text/html" allowFullScreen={true}/>
+  </Ratio>
+    
+    </Col>
+    <Col md={{ span: 4 }}>Second, but last</Col>
   </Row>
 </Container>
+</>
 )};
